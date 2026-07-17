bbf0ed01661e345c38f60215e10837f544f834fd
Ilya Eliseev <ie@e-ise.com>
2026-07-17 18:42:55 +0300

    async

--- 04-async-event_loop.py
+++ 04-async-event_loop.py
@@ -1,11 +1,13 @@
 import asyncio
 
-async def worker():
-    print("Старт")
+async def worker(name):
+    print(f"Старт {name}")
 
     await asyncio.sleep(2)
 
-    print("Конец")
+    print(f"Конец {name}")
 
 
-asyncio.run(worker())
+asyncio.run(worker("Worker 1"))
+
+
